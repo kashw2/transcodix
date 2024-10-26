2d364deced53e793f13dbd0eab00db8d0491cdb7
kashw2 <supra4keanu@hotmail.com>
2024-10-26 12:22:05 +1000

    feat(module): added support for user setting

--- modules/module.nix
+++ modules/module.nix
@@ -32,6 +32,12 @@ in
       example = "/home/user/Downloads";
     };
 
+    user = lib.mkOption {
+        type = lib.types.str;
+        default = "transcodix";
+        example = "kashw2";
+    };
+
     transcodingPackage = lib.mkOption {
       type = lib.types.either lib.types.str (
         lib.types.enum [
@@ -55,6 +61,11 @@ in
         Restart = "always";
         RestartSec = "10s";
       };
+    };
+
+    users.users.${cfg.user} = lib.mkIf cfg.user != "" {
+        description = "System user for the transcodix instance";
+        isSystemUser = true;
     };
   };
 }
